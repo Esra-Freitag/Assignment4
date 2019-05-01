d0f3ebf2c2f2dc2ee9dc847088ec2f9758d1eba2
husbkk <husb@live.hk>
2019-05-01 15:11:28 +0200

    Add files via upload

--- src/svg/shape/Line.java
+++ src/svg/shape/Line.java
@@ -6,36 +6,36 @@ public class Line extends Shape{
 	public boolean load(String expr) {
 		if (expr.contains(" x1="))
 		{
-			 	 final Double result = SVGParser.extractDouble(expr, " x1=");
-			 	 if (result != null)
-			 	 	 x1 = result.doubleValue();
+			final Double result = SVGParser.extractDouble(expr, " x1=");
+			if (result != null)
+			x1 = result.doubleValue();
 		} else {
 			return false;
 		}
 		
 		if (expr.contains(" y1="))
 		{
-			 	 final Double result = SVGParser.extractDouble(expr, " y1=");
-			 	 if (result != null)
-			 	 	 y1 = result.doubleValue();
+			final Double result = SVGParser.extractDouble(expr, " y1=");
+			if (result != null)
+			y1 = result.doubleValue();
 		} else {
 			return false;
 		}
 		
 		if (expr.contains(" x2="))
 		{
-			 	 final Double result = SVGParser.extractDouble(expr, " x2=");
-			 	 if (result != null)
-			 	 	 x2 = result.doubleValue();
+			final Double result = SVGParser.extractDouble(expr, " x2=");
+			if (result != null)
+				x2 = result.doubleValue();
 		} else {
 			return false;
 		}
 		
 		if (expr.contains(" y2="))
 		{
-			 	 final Double result = SVGParser.extractDouble(expr, " y2=");
-			 	 if (result != null)
-			 	 	 y2 = result.doubleValue();
+			final Double result = SVGParser.extractDouble(expr, " y2=");
+			if (result != null)
+			y2 = result.doubleValue();
 		} else {
 			return false;
 		}
--- src/svg/shape/Rect.java
+++ src/svg/shape/Rect.java
@@ -6,54 +6,54 @@ public class Rect extends Shape{
 	public boolean load(String expr) {
 		if (expr.contains(" x="))
 		{
-			 	 final Double result = SVGParser.extractDouble(expr, " x=");
-			 	 if (result != null)
-			 	 	 x = result.doubleValue();
+			final Double result = SVGParser.extractDouble(expr, " x=");
+			if (result != null)
+			x = result.doubleValue();
 		} else {
 			return false;
 		}
 		
 		if (expr.contains(" y="))
 		{
-			 	 final Double result = SVGParser.extractDouble(expr, " y=");
-			 	 if (result != null)
-			 	 	 y = result.doubleValue();
+			final Double result = SVGParser.extractDouble(expr, " y=");
+			if (result != null)
+			y = result.doubleValue();
 		} else {
 			return false;
 		}
 		
 		if (expr.contains(" rx="))
 		{
-			 	 final Double result = SVGParser.extractDouble(expr, " rx=");
-			 	 if (result != null)
-			 	 	 rx = result.doubleValue();
+			final Double result = SVGParser.extractDouble(expr, " rx=");
+			if (result != null)
+			rx = result.doubleValue();
 		} else {
 			return false;
 		}
 		
 		if (expr.contains(" ry="))
 		{
-			 	 final Double result = SVGParser.extractDouble(expr, " ry=");
-			 	 if (result != null)
-			 	 	 ry = result.doubleValue();
+			final Double result = SVGParser.extractDouble(expr, " ry=");
+			if (result != null)
+			ry = result.doubleValue();
 		} else {
 			return false;
 		}
 		
 		if (expr.contains(" width="))
 		{
-			 	 final Double result = SVGParser.extractDouble(expr, " width=");
-			 	 if (result != null)
-			 	 	 width = result.doubleValue();
+			final Double result = SVGParser.extractDouble(expr, " width=");
+			if (result != null)
+			width = result.doubleValue();
 		} else {
 			return false;
 		}
 		
 		if (expr.contains(" height="))
 		{
-			 	 final Double result = SVGParser.extractDouble(expr, " height=");
-			 	 if (result != null)
-			 	 	 height = result.doubleValue();
+			final Double result = SVGParser.extractDouble(expr, " height=");
+			if (result != null)
+			height = result.doubleValue();
 		} else {
 			return false;
 		}
