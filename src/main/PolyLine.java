a7b9a25020f7e72330d396e4e2273b0ba7ccb957
Exodia7 <valentin.ringlet@gmail.com>
2019-05-01 15:26:21 +0200

    add load() in PolyLine

--- src/main/PolyLine.java
+++ src/main/PolyLine.java
@@ -36,6 +36,46 @@ public class PolyLine extends Shape {
 		addPoint(new Point2D(x, y));
 	}
 	
+	/** Load a string of form "(x1,y1) (x2,y2) (x3,y3) (x4,y4)"
+	*/
+	@Override
+	public boolean load(String expr) {
+		while (expr.length() > 0) {
+			//Retrieve the x-coordinate
+			Double resX = SVGParser.extractDouble(expr, "(");
+			
+			//If a double value was found 
+			if (resX != null) {
+				//And save it under x
+				double x = resX.doubleValue();
+			
+				//Retrieve the y-coordinate
+				Double resY = SVGParser.extractDouble(expr, ",");
+				
+				//If a double value was found
+				if (resY != null) {
+					//And save it under y
+					double y = resY.doubleValue();
+					
+					//Add a point x, y
+					points.addPoint(x, y);
+					
+					//Cut the String, leave out the part of the current point
+					double cutIndex = expr.indexOf(" ") + 1;
+					expr = expr.substring(cutIndex);
+				}
+				else {		//no double value found by SVGParser.extractDouble for the y-value
+					return false;
+				}
+			}
+			else {	//no double value found by SVGParser.extractDouble for the x-value
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
 	/** Creates a new PolyLine object with the same contents as this Object
 	
 		@return the new PolyLine object with the same contents
